73d63a267a218529531c5de9cde87cc870b37cf0
ARSALASLT137\Arsala Dilshad <arsala.dilshad@hotmail.com>
2024-09-09 15:24:36 +0200

    First Commit

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const FIRST_NAME = 'Test';
+export const LAST_NAME =    "User";
+export const PHONE_NUMBER = "+46726415913";
+export const EMAIL = "user@facebook.com";
+export const PASSWORD = "12";
+export const COUNTRY_NAME = "Sweden";
+export const COMPANY_NAME = "Facebook";
+export const INFORMATION_MEDIA = "LinkedIn";
